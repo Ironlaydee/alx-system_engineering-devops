605a9f102508e4c5e55e7fb7bc0e300061f006be
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-21 15:42:02 -0700

    create a puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Use puppet to create a file in /tmp.
 
 file { '/tmp/school':
-    group => 'www-data',
-    owner => 'www-data',
+    group   => 'www-data',
+    owner   => 'www-data',
     content => 'I love Puppet',
-    mode => '0744',
-    ensure => 'file',
+    mode    => '0744',
+    ensure  => 'file',
 }

bd8cae81d526fc07c17d51ac03c58cbe85ed28db
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-21 14:10:25 -0700

    config manager

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Use puppet to create a file in /tmp.
+
+file { '/tmp/school':
+    group => 'www-data',
+    owner => 'www-data',
+    content => 'I love Puppet',
+    mode => '0744',
+    ensure => 'file',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,2 @@
+# 0x0A configuration_management
+This is a readme for xomfiguration management

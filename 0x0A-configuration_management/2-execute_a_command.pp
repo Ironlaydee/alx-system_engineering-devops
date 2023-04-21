c9b7bd6f7c33672b0117bfddeb062f76bb9753ce
Adedolapo Adefaye <daintiestdolly@gmail.com>
2023-04-21 16:00:19 -0700

    killitnow

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # using puppet, kill the process called kill me now
 exec { 'killmenow':
   command => '/usr/bin/pkill killmenow',
-  path => '/bin/',
-  returns => [0, 1],
-}
+  path    => '/bin/',
+  returns => [0, 1]
+  } 

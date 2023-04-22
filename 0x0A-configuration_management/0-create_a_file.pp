f5578334daa89616d37eaed1de900a229ba038ae
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-04-22 01:20:10 +0100

    creating a puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Use puppet to create a file in /tmp
-
-file { '/tmp/school' :
-  ensure  => 'file', 
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-  }
+file { '/tmp/school' : 
+    ensure  => 'file',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
